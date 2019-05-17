d75c5d531c9c6294663ae61609edd18a527fc5de
hty007 <hty007@yandex.ru>
2019-05-17 14:57:05 +0300

    Начал делать сумму.

---
+++ nums/nums/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace nums
+{
+    class Program
+    {
+        static double[] data;
+
+        static int[] Enumeration(double target)
+        {
+            var buf = new List<int>();
+			double sum = 0;
+            while (sum != target)
+			{
+				if(sum<target)
+				{
+					
+				}
+				
+			}
+			return buf.ToArray();
+		}
+
+        static double Summ( IEnumerable<int> nums)
+        {
+            double sum = 0;
+            foreach (int index in nums)
+            {
+                sum += data[index];
+            }
+            return sum;
+        }
+
+
+        static void LoadData()
+        {
+            using (StreamReader sr = new StreamReader("nums.txt"))
+            {
+                var d =new  List<double>();
+                while (sr.EndOfStream)
+                {
+                    d.Add(double.Parse(sr.ReadLine()));
+                }
+                data = d.ToArray();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
