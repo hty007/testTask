aafaf591b1d0b846dcf0ff19a8bfa0a58485f293
hty007 <hty007@yandex.ru>
2019-05-17 13:26:02 +0300

    Еще одно тестовое задание

---
+++ personal/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new ModelPersons();
+            var o = (ModelPersons)DataContext;
+            ListPerson.ItemsSource = o.FilterPersons;
+            PostsList.ItemsSource = o.Posts;
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FilterGo_Click(object sender, RoutedEventArgs e)
+        {
+            var o = (ModelPersons)DataContext;
+            o.ApplyFilter();
+            
+            //ListPerson.ItemsSource = o.FilterPersons;
+        }
+
+        private void Filter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                var o = (ModelPersons)DataContext;
+                if (Filter.Text == "")
+                {
+                    o.SalaryFilter = 0;
+                    return;
+                }
+                o.SalaryFilter = double.Parse(Filter.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    string.Format("Что-то пошло не так! \n{0}", ex.Message),
+                    "Exception",
+                    MessageBoxButton.OK);
+            }
+        }
+
+        private void SortUpCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            var o = (ModelPersons)DataContext;
+            o.SortUp = (bool)SortUpCheck.IsChecked;
+        }
+
+        private void SortUpCheck_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var o = (ModelPersons)DataContext;
+            o.SortUp = (bool)SortUpCheck.IsChecked;
+        }
+
+        private void ClearFilter_Click(object sender, RoutedEventArgs e)
+        {
+            PostsList.SelectedIndex = -1;
+            Filter.Text = "";
+            SortUpCheck.IsChecked = false;
+            //var o = (ModelPersons)DataContext;
+            FilterGo_Click(sender, e);
+        }
+
+        private void PostsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //var o = (ModelPersons)DataContext;
+            //o.PostFilter = PostsList.SelectedValue;
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                       string.Format(@"Приложение должно иметь:
+1.Список сотрудников, выводит все данные сотрудника.Автоматически упорядочивается по уменьшению зарплаты
+2.Выпадающий список, которые содержит все имеющиеся должности
+3.Поле ввода, служит для фильтрации сотрудников по зарплате(больше или равно)
+4.Кнопка применения фильтра.При нажатии на кнопку, список сотрудников отфильтровывается по выбранной должности из выпадающего списка(2) и зарплате из поля(3)
+5.Кнопка сброса фильтра"),
+                       "Тестовое задание 1.0",
+                       MessageBoxButton.OK);
+
+        }
+    }
+}
---
+++ personal/WpfApp1/ModelPersons.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace WpfApp1
+{
+
+    public 
+        class ModelPersons
+    {
+        public ObservableCollection<Person> FilterPersons { get; private set; }
+        [DataMember]
+        public List<Person> AllPersons { get; private set; }
+        public ObservableCollection<string> Posts { get; private set; }
+        public bool SortUp { get; set; }
+        public string PostFilter { get; set; }
+        public double SalaryFilter { get; set; }
+
+        public ModelPersons(bool q)
+        {
+            if (q)
+            {
+                AllPersons = new List<Person>();
+                Posts = new ObservableCollection<string>();
+            }
+        }
+
+        public ModelPersons(List<Person> persons)
+        {
+            AllPersons = persons;
+            PostUpdate();
+        }
+
+        public ModelPersons()
+        {
+            PostFilter = "";
+            SalaryFilter = 0;
+            SortUp = false;
+            Posts = new ObservableCollection<string>();
+            FilterPersons = new ObservableCollection<Person>();
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<Person>));
+            using (FileStream fs = new FileStream("AllPersons.json", FileMode.Open))
+            {
+                AllPersons = (List<Person>)jsonFormatter.ReadObject(fs);
+            }
+            PostUpdate();
+
+            /*
+            AllPersons = new List<Person>();
+            AddPerson(new Person("Иван Максимов", "Кассир", 15000, 2.5));
+            AddPerson(new Person("Митрофан Боголюбов", "Менеджер", 35000, 3.6));
+            AddPerson(new Person("Виталий Костенко", "Кассир", 16000, 1.9));
+            AddPerson(new Person("Максим Романов", "Кассир", 20000, 5.4));//*/
+            ApplyFilter();
+        }
+
+        private void PostUpdate()
+        {
+            foreach (var p in AllPersons)
+            {
+                if (Posts.IndexOf(p.Post) < 0)
+                    Posts.Add(p.Post);
+            }
+        }
+
+        ~ModelPersons()
+        {
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<Person>));
+            using (FileStream fs = new FileStream("AllPersons.json", FileMode.Create))
+            {
+                jsonFormatter.WriteObject(fs, AllPersons);
+            }
+        }
+
+
+        public void AddPerson(Person person)
+        {
+            if (AllPersons.IndexOf(person) < 0)
+                AllPersons.Add(person);
+            if (Posts.IndexOf(person.Post) < 0)
+                Posts.Add(person.Post);
+            
+            AllPersons.Sort(ComparePerson);
+        }
+
+        private int ComparePerson(Person A, Person B)
+        {
+            var resutl = A.Salary.CompareTo(B.Salary);
+            return (SortUp)? resutl : resutl*(-1);
+
+        }
+
+        // События
+        public void ApplyFilter()
+        {
+            AllPersons.Sort(ComparePerson);
+            var salary = SalaryFilter;
+            if (salary == 0)
+                salary = double.MinValue;
+            FilterPersons.Clear();
+            if (PostFilter == ""|| PostFilter == null)
+            {
+                foreach (var p in AllPersons)
+                {
+                    if (salary <= p.Salary)
+                        FilterPersons.Add(p);
+                }
+            }
+            else
+            {
+
+                foreach (var p in AllPersons)
+                {
+                    if (p.Post == PostFilter && salary <= p.Salary)
+                        FilterPersons.Add(p);
+                }
+            }            
+        }
+
+
+
+    }
+}
