0bade1372717b3be8b6e7224316f95b8b08d4ab7
hty007 <47690087+hty007@users.noreply.github.com>
2020-07-14 17:29:02 +0300

    добавлен пример 3 и 4

--- Algorithms/Algorithms/starter/exercises/ex1.cs
+++ Algorithms/Algorithms/starter/exercises/ex1.cs
@@ -9,6 +9,10 @@ namespace sterter.exercises
 {
     public class Exercise1 : SubCmd, ICmd
     {
+        private const string EXAMPLE1_TEXT = "Пример 1.\nДокажите что значение <a+b> может быть больше, чем значение min(a,b).";
+        private const string EXAMPLE2_TEXT = "Пример 2.\nДокажите что значение <a*b> может быть больше, чем значение min(a,b).";
+        private const string EXAMPLE3_TEXT = "Пример 3.\nНачертите сеть дорог с двумя точками а и b, такими, что маршрут между ними, преодолеваемый за кратчайшее время, не является самым коротким.";
+        private const string EXAMPLE4_TEXT = "Пример 4.\nНачертите сеть дорог с двумя точками а и b, самый короткий маршрут между которыми не является маршрутом с наименьшим количеством поворотов.";
         #region Интерфейст ICmd
         public string GetHelp()
         {
@@ -23,15 +27,16 @@ namespace sterter.exercises
         public Exercise1()
         {
             Hеader = "Упражнение 1.7";
-            AddSubCommand("ex1", "Пример 1. Докажите что значение <a+b> может быть больше, чем значение min(a,b).", example1);
-            AddSubCommand("ex2", "Пример 2. Докажите что значение <a*b> может быть больше, чем значение min(a,b).", example2);
+            AddSubCommand("ex1", EXAMPLE1_TEXT, example1);
+            AddSubCommand("ex2", EXAMPLE2_TEXT, example2);
+            AddSubCommand("ex3", EXAMPLE3_TEXT, example3);
+            AddSubCommand("ex4", EXAMPLE4_TEXT, example4);
         }
 
         private void example1(string obj)
         {
             #region Заглавие и параметры
-            Console.WriteLine("Упражнение 1.7 Пример 1");
-            Console.WriteLine("Докажите что значение <a+b> может быть больше, чем значение min(a,b).");
+            Console.WriteLine("Упражнение 1.7 "+ EXAMPLE1_TEXT);
 
             int min = -10;
             int max = 100;
@@ -87,8 +92,7 @@ namespace sterter.exercises
         private void example2(string obj)
         {
             #region Заглавие и параметры
-            Console.WriteLine("Упражнение 1.7 Пример 2");
-            Console.WriteLine("Докажите что значение <a*b> может быть больше, чем значение min(a,b).");
+            Console.WriteLine("Упражнение 1.7 " + EXAMPLE2_TEXT);
 
             int min = -5;
             int max = 10;
@@ -143,10 +147,50 @@ namespace sterter.exercises
             if (find == 0) Console.WriteLine("Ничего не найдено");
             #endregion
         }
-
         private static bool Check_ex2(int a, int b)
         {
             return a * b < Math.Min(a, b);
+        }
+        private void example3(string obj)
+        {
+            //Начертите сеть дорог с двумя точками а и Ь, такими, что маршрут между ними, 
+            //преодолеваемый за кратчайшее время, не является самым коротким.
+            #region Заглавие и параметры
+            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Упражнение 1.7 " + EXAMPLE3_TEXT);
+            #endregion
+            #region Ответ
+            string answer = @"Данную сеть дорог можно реализовать в взвешанном однонаправленом графе:
+A→→
+↑ ↓
+↑ ↓
+B←←
+Так в приведённом примере путь из 'А' в 'В' составить 6 условных единиц, а из 'В' в 'А' всего 2.
+";
+            Console.WriteLine(answer);            
+            #endregion
+        }
+        private void example4(string obj)
+        {
+            //Начертите сеть дорог с двумя точками а и Ь, самый короткий маршрут между 
+            //которыми не является маршрутом с наименьшим количеством поворотов.
+            #region Заглавие и параметры
+            Console.InputEncoding = Encoding.UTF8;
+            Console.WriteLine("Упражнение 1.7 " + EXAMPLE4_TEXT);
+            #endregion
+            #region Ответ
+            string answer = @"Данную сеть дорог можно реализовать в взвешанном однонаправленом графе:
+  1   2  
+A→→→→→→
+↑ ↓→↓ ↓
+↑   ↓ ↓
+↑ ↓←↓ ↓
+B←←←←←←
+Так в приведённом примере путь из 'А' в 'В' при повороте на дорогу 1 составить 11 условных единиц и встретится 6 поворотов, 
+а при повороте на дорогу 2 будет потрачено 15 условных едениц и встретится всего 2 поворота.
+";
+            Console.WriteLine(answer);
+            #endregion
         }
     }
 
