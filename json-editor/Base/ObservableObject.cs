627fa28d954fe5ce2949ae69051e8373e68b8d93
ренат <hty007@yandex.ru>
2021-06-21 19:07:43 +0300

    Create Template

---
+++ json-editor/Base/ObservableObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace WPFStorage.Base
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Установить значение хранящего поля свойства и вызвать событие <see cref="PropertyChanged"/> при его обновлении.
+        /// </summary>
+        /// <typeparam name="T">Тип свойства.</typeparam>
+        /// <param name="backField">Поле, хранящее значение свойства.</param>
+        /// <param name="value">Новое значение свойства.</param>
+        /// <param name="propertyName">Имя свойства.</param>
+        /// <returns>true, если значение действительно было установлено.</returns>
+        protected virtual bool SetProperty<T>(ref T backField, T value, /*[CallerMemberName]*/ string propertyName = null)
+        {
+            // Из-за ограничения .net 4.0 задании атрибут [CallerMemberName] не функционирует,
+            // по этому пришлось испльзовать конструкцию представленную ниже
+            if (propertyName == null)
+            {
+                StackTrace stackTrace = new StackTrace();
+                StackFrame frame = stackTrace.GetFrame(1);
+                MethodBase method = frame.GetMethod();
+                propertyName = method.Name.Replace("set_", "");
+            }
+
+            if (EqualityComparer<T>.Default.Equals(backField, value))
+                return false;
+            backField = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ json-editor/Dialogs/SelectorWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using WPFStorage.Base;
+
+namespace WPFStorage.Dialogs
+{
+    /// <summary>
+    /// Логика взаимодействия для SelectorWindow.xaml
+    /// </summary>
+    public partial class SelectorWindow : Window
+    {
+        public SelectorWindow(SelectorViewModel model)
+        {
+            InitializeComponent();
+            Closing += (e, o) => { DialogResult = model.Select != null; };
+            model.Close += () => Close();
+            DataContext = model;
+            Loaded += (a, b) => { model.Initialize(); };
+        }
+    }
+
+    public class SelectorViewModel: ObservableObject
+    {
+        private string title = "Окно ввода";
+        public string Title { get => title; set { title = value; OnPropertyChanged(); } }
+
+        private string question = "Вопрос";
+        public string Question { get => question; set { question = value; OnPropertyChanged(); } }
+
+        List<string> items;
+        public List<string> Items { get => items; set { items = value; OnPropertyChanged(); } }
+
+        public RelayCommand<string> SelectItem { get; }
+        public string Select { get; private set; }
+        public bool IsResult { get; internal set; }
+        public int Timeout { get; internal set; }
+
+        internal Action Close;
+        private DispatcherTimer timer;
+
+        public SelectorViewModel()
+        {
+            SelectItem = new RelayCommand<string>(SelectItemMethod);
+           
+        }
+
+        private void SelectItemMethod(string obj)
+        {
+            Select = obj;
+            Close?.Invoke();
+        }
+
+        internal void Initialize()
+        {
+            if (Timeout > 0)
+            {
+                timer = new DispatcherTimer();
+                timer.Interval = new TimeSpan(Timeout * TimeSpan.TicksPerMillisecond);
+                timer.Tick += Timer_Tick;
+                timer.Start();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            Close?.Invoke();
+        }
+    }
+}
